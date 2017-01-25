3d5be6ba7cc8cdef7b73f873c0c92fdb79cc7761
LehnenJ <justin.lehnen@gmx.de>
2017-01-25 16:20:57 +0100

    Timer fix, presets, general sophistication

--- games/ft.js
+++ games/ft.js
@@ -43,8 +43,8 @@ string = betOptions.join(' vs ');
 		pot = 0,
 		options = [],
 		table = [],
-		betTimerStatus = $.getSetIniDbBoolean('ftBetSettings', 'betTimer', false),
-		betTimer = $.getSetIniDbNumber('ftBetSettings', 'betTimerSeconds', 120);
+		betTimerStatus = $.getSetIniDbBoolean('ftBetSettings', 'betTimerStatus', false),
+		betTimer = $.getSetIniDbNumber('ftBetSettings', 'betTimer', 120);
 		timeOut = null;
 
 	/**
@@ -124,7 +124,7 @@ string = betOptions.join(' vs ');
 			*/
 			} else if (action.equalsIgnoreCase('toggletimer')) {
 				betTimerStatus = !betTimerStatus;
-				$.inidb.set('ftBetSettings', 'betTimer', betTimerStatus);
+				$.inidb.set('ftBetSettings', 'betTimerStatus', betTimerStatus);
 
 				$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.notification.timer.toggle.' + (betTimerStatus ? 'on' : 'off')));
 				return;
@@ -142,10 +142,10 @@ string = betOptions.join(' vs ');
 				}
 
 
-				betTimerSeconds = parseInt(args[1]);
-				$.inidb.set('ftBetSettings', 'betTimer', betTimerSeconds);
+				betTimer = parseInt(args[1]);
+				$.inidb.set('ftBetSettings', 'betTimer', betTimer);
 
-				$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.notification.timer.set.' + (betTimerStatus ? 'on' : 'off'), betTimerSeconds));
+				$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.notification.timer.set.' + (betTimerStatus ? 'on' : 'off'), betTimer));
 				return;
 
 			/**
@@ -200,6 +200,28 @@ string = betOptions.join(' vs ');
 				} else if (subAction.equalsIgnoreCase('open')) {
 					openBet(event);
 					return;
+
+				/**
+				 * @commandpath ft gamble openpreset [name] - Opens a saved bet.
+				*/
+				} else if (subAction.equalsIgnoreCase('openpreset')) {
+					openPreset(sender, args[2]);
+					return;
+
+				/**
+				 * @commandpath ft gamble savepreset [name] - Saves a running bet.
+				*/
+				} else if (subAction.equalsIgnoreCase('savepreset')) {
+					savePreset(sender, args[2]);
+					return;
+
+				/**
+				 * @commandpath ft gamble deletepreset [name] - Deletes a saved bet.
+				*/
+				} else if (subAction.equalsIgnoreCase('deletepreset')) {
+					deletePreset(sender, args[2]);
+					return;
+
 
 				/**
 				 * @commandpath ft gamble cancel - Cancels a pending bet and returns the points.
@@ -263,10 +285,19 @@ string = betOptions.join(' vs ');
 				$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.running'));
 				return;
 			}
+        }
+
+        // parse wager
+ 		if (amount.toUpperCase() == 'ALL') {
+    		wager = $.getUserPoints(sender);
+		} else if (amount.toUpperCase() == 'MAX') {
+    		wager = limit($.getUserPoints(sender), maximumBet);
+		} else if (amount.toUpperCase() == 'MIN') {
+    		wager = limit($.getUserPoints(sender), minimumBet);
+		} else {
+			wager = expandPointSuffix(amount);
 		}
 
-
-		wager = (amount.toUpperCase() == 'ALL') ? $.getUserPoints(sender) : expandPointSuffix(amount);
 		choice = parseInt(option) - 1;
 
 
@@ -327,10 +358,7 @@ string = betOptions.join(' vs ');
 
 	/** 
 	 * @function openBet
-	 * @param {Number} min
-	 * @param {Number} max
-	 * @param {Array} payouts
-	 * @param {Array} options
+	 * @param {*} event
 	 */
 	function openBet(event) {
 		var sender = event.getSender().toLowerCase(),
@@ -379,6 +407,122 @@ string = betOptions.join(' vs ');
 		$.say($.whisperPrefix(sender) + output);
 	};
 	/** 
+	 * @function openPreset
+	 * @param {string} sender
+	 * @param {string} name
+	 */
+	function openPreset(sender, name) {
+		var payouts = [],
+			choices = [],
+			output = $.lang.get('pdbetting.notification.bet.opened');
+
+    	// file given?
+		if (!name) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.usage.ft.gamble.preset.open'));
+			return;
+		}
+
+		// file exists?
+		if (!$.fileExists('./addons/pdBetSystem/' + name + '.txt')) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.preset.404'));
+			return;
+		}
+
+		var contents = $.readFile('./addons/pdBetSystem/' + name + '.txt').join('').split(';');
+
+		// sufficient parameters in file?
+		if (contents.length != 4) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.preset.corrupt'));
+			return;
+		}
+
+		payouts = contents[2].split(',');
+		choices = contents[3].split(',');
+
+		// Outtake from openBet()
+		for(i in payouts) {
+			options[i] = {
+				payout: payouts[i],
+				option: choices[i].toLowerCase().trim()
+			};
+		}
+
+		minimumBet = contents[0];
+		maximumBet = contents[1];
+		status = true;
+
+		if (betTimerStatus) {
+			output += ' ' + $.lang.get('pdbetting.notification.bet.timer', betTimer);
+
+			timeOut = setTimeout(function() {
+				closeBet('');
+			}, betTimer * 1000);
+		}
+
+		$.say($.whisperPrefix(sender) + output);
+	};
+	/** 
+	 * @function savePreset
+	 * @param {string} sender
+	 * @param {string} name
+	 */
+	function savePreset(sender, name) {
+		var payouts = '',
+			choices = '',
+			result = '';
+
+    	// bet open?
+		if (!status) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.bet.404'));
+			return;
+		}
+
+		// file given?
+		if (!name) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.usage.ft.gamble.preset.save'));
+			return;
+		}
+
+		// file already exists
+		if ($.fileExists('./addons/pdBetSystem/' + name + '.txt')) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.preset.exists'));
+			return;
+		}
+
+		for(i in options) {
+   			payouts += options[i].payout + ',';
+   			choices += options[i].option + ',';
+		}
+
+		payouts = payouts.slice(0, -1);
+		choices = choices.slice(0, -1);
+
+		result = minimumBet + ';' + maximumBet + ';' + payouts + ';' + choices;
+		
+		$.mkDir('./addons/pdBetSystem/');
+		$.writeToFile(result, './addons/pdBetSystem/' + name + '.txt', false);
+	};
+	/** 
+	 * @function deletePreset
+	 * @param {string} sender
+	 * @param {string} name
+	 */
+	function deletePreset(sender, name) {
+	    // file given?
+		if (!name) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.usage.ft.gamble.preset.delete'));
+			return;
+		}
+
+		// file exists?
+		if (!$.fileExists('./addons/pdBetSystem/' + name + '.txt')) {
+			$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.err.preset.404'));
+			return;
+		}
+
+		$.deleteFile('./addons/pdBetSystem/' + name + '.txt', true);
+	};
+	/** 
 	 * @function closeBet
 	 * @param {Array} sender
 	 */
@@ -392,9 +536,9 @@ string = betOptions.join(' vs ');
 		}
 
 		closed = true;
-		clearTimeout(timeOut);
+		if (timeOut != null) clearTimeout(timeOut);
 
-		$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.notification.bet.closed'));
+		$.say((sender != '') ? $.whisperPrefix(sender) : '' + $.lang.get('pdbetting.notification.bet.closed'));
 		printInfo(sender, true);
 	};
 	/** 
@@ -497,7 +641,7 @@ string = betOptions.join(' vs ');
 			points = 0;
 		}
 
-		$.say($.whisperPrefix(sender) + $.lang.get('pdbetting.notification.bet.info.options', result.slice(0, -2)));
+		$.say((sender != '') ? $.whisperPrefix(sender) : '' + $.lang.get('pdbetting.notification.bet.info.options', result.slice(0, -2)));
 	};
 
 	/**
@@ -513,8 +657,8 @@ string = betOptions.join(' vs ');
 		pot = 0;
 		options = [];
 		table = [];
-		betTimerStatus = $.getSetIniDbBoolean('ftBetSettings', 'betTimer', false);
-		betTimer = $.getSetIniDbNumber('ftBetSettings', 'betTimerSeconds', 120);
+		betTimerStatus = $.getSetIniDbBoolean('ftBetSettings', 'betTimerStatus', false);
+		betTimer = $.getSetIniDbNumber('ftBetSettings', 'betTimer', 120);
 		
 		if (timeOut != null) clearTimeout(timeOut);
 	};
@@ -526,23 +670,25 @@ string = betOptions.join(' vs ');
 	*/
 	function expandPointSuffix(pointsString) {
 		// m (million), b (billion), t (trillion), Q (Quadrillion)
-		var factors = [1000000, 1000000000, 1000000000000, 1000000000000000],
-			suffix = pointsString.slice(-1),
+		var map = [],
+			suffix = str.slice(-1),
 			temp = -1,
 			result = -1;
 
-		if (!isNaN(pointsString.slice(0, -1))) {
-			temp = parseFloat(pointsString.slice(0, -1));
+		map['m'] = 1000000;
+		map['b'] = 1000000000;
+		map['t'] = 1000000000000;
+		map['Q'] = 1000000000000000;
 
-			if (suffix == 'm') result = temp * factors[0];
-			else if (suffix == 'b') result = temp * factors[1];
-			else if (suffix == 't') result = temp * factors[2];
-			else if (suffix == 'Q') result = temp * factors[3];
-			else result = parseFloat(pointsString);
+   		if (!isNaN(str.slice(0, -1))) {
+      		temp = parseFloat(str.slice(0, -1));
 
-		}
+      		if (map[suffix] != undefined) {
+         		result = temp * map[suffix];
+      		}
+   		}
 
-		return result;
+   		return result;
 	};
 
 	/**
@@ -562,6 +708,19 @@ string = betOptions.join(' vs ');
 
 		return result;
 	};
+
+	/**
+	 * @function limit
+	 * @param {Number} value
+	 * @param {Number} limit
+	 * @returns {Number}
+	*/
+	function limit(value, limit) {
+		if (value < 0) value = 0;
+		else if (value > limit) value = limit;
+
+		return value;
+	}
 
 	/** Export functions to API */
 	$.expandPointSuffix = expandPointSuffix;
